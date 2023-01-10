9965bc456e5d64f3de06d24b47b9f3395e3a81b0
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-01-10 08:49:16 +0530

    Fixed goerli network url endpoint + deployed smart contract on georli testnet

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -1,12 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    georli: {
-      url: process.env.ALCHEMY_GOERLI_URL,
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI_URL}`,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     },
   },
